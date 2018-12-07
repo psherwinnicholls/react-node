68f715626c4b94322c2022783ad5b6836c532b56
psherwinnicholls <psherwinnicholls@gmail.com>
2018-12-07 14:10:07 +0000

    first push

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+export default class App extends Component {
+  state = {
+    data: {people:[]}
+  };
+
+  componentDidMount() {
+     fetch('/api')
+       .then(res => res.json())
+       .then(data => this.setState({ data }));
+   }
+
+  render() {
+    return (
+      <div className="App">
+
+        {/* Iterate over the JSON api */}
+        {this.state.data.people.map((person, i) => {
+
+          {/* What to render */}
+          return(
+            <div key={i}>
+            
+              {/* Render the name */}
+              <h1>{person.name}</h1>
+
+              {/* Create a random age */}
+              <p>{Math.floor(18 + (41 * Math.random()))}</p>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
